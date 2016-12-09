c0ee11158e5afc93e406e8f18b4505def2f558a4
Michael Kulinski <me@michaelkulinski.com>
2016-12-09 11:01:49 -0800

    removed and added styles

---
+++ src/stylesheets/mainStyle.js
@@ -0,0 +1,15 @@
+const darkGold = '#8c774b';
+const lightGold = '#aa9260';
+const textGrey = '#707070';
+const textBlack = '#282828';
+
+module.export = {
+  headings: {
+    color: lightGold,
+    fontFamily: 'Open Sans, sans-serif',
+  },
+  text: {
+    color: textBlack,
+    fontFamily: 'Roboto, sans-serif',
+  },
+};
