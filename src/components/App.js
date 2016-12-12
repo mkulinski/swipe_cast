f52588cca597b02c965a6c8e95360f3e99b95dbf
Michael Kulinski <me@michaelkulinski.com>
2016-12-12 11:14:54 -0800

    added Nav and have the base structure for the app working

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import Nav from './Nav';
-import styles from '../stylesheets/mainStyle';
 
 const App = () => {
   return (
     <div>
       <Nav />
-      <h1 style={styles.text}>
-        Main App Container
-      </h1>
+      <h1>Main App Container</h1>
     </div>
   );
 };
---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>Profile</li>
+        <li>Login/Log out</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Nav;
