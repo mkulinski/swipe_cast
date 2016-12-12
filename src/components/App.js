f47bfaa095d2a05ba6c715cd1ee3d19d9bd221ad
Michael Kulinski <me@michaelkulinski.com>
2016-12-12 11:00:11 -0800

    decided on structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Nav from './Nav';
 import styles from '../stylesheets/mainStyle';
 
 const App = () => {
   return (
-    <div style={styles.text}>
-      Main App Container
+    <div>
+      <Nav />
+      <h1 style={styles.text}>
+        Main App Container
+      </h1>
     </div>
   );
 };
